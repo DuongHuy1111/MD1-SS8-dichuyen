2dd4e4312f957674e65874bcb47669e0eb76b70d
DuongHuy1111 <huy.alex.1111@gmail.com>
2019-03-20 21:04:01 +0700

    Add .

---
+++ script.js
@@ -0,0 +1,35 @@
+function Hero(image, top, left, size){
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+
+    this.getHeroElement = function(){
+        return '<img width="'+ this.size + '"' +
+            ' height="'+ this.size + '"' +
+            ' src="' + this.image +'"' +
+            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+    }
+
+    this.moveRight = function(){
+        this.left += 20;
+        console.log('ok: ' + this.left);
+        
+    this.moveLeft = function () {
+        this.left -= 20;
+    }
+    }
+
+}
+
+let hero = new Hero('goku.png', 200, 30, 200);
+
+function start(){
+    if(hero.left < window.innerWidth - hero.size){
+        hero.moveRight();
+    }
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    setTimeout(start, 100)
+}
+
+start();
